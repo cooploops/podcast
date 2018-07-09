7159e26576f1d0b4d1adbdfbc3a67d6512927e5b
TJ <cooploops@gmail.com>
2018-07-08 23:54:14 -0700

    still need to fix body scrolling on banner click or focus

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
 import { Container, Responsive, Image } from 'semantic-ui-react';
+import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
 import Slider from 'react-slick';
 
 class Banner extends Component {
+
     render(){  
         const settings = {
             dots: true,
@@ -17,6 +19,7 @@ class Banner extends Component {
             'marginBottom':'3em'
         }
 
+
         //could just make an array of images and map over it
         // export carousel to it's own component and import into Banner
         return (
@@ -24,7 +27,14 @@ class Banner extends Component {
                 <Slider {...settings}>
                     {this.props.images.map((path, i) => (
                         <div key={i}>
-                            <Image centered={true} rounded={true} src={path} alt={'cat'} key={i} fluid />
+                            <Image 
+                            centered={true} 
+                            rounded={true} 
+                            src={path} 
+                            alt={'cat'} 
+                            key={i} 
+                            fluid
+                           />
                         </div>
                     ))}
                 </Slider>
