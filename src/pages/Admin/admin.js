af42f075705f31642beba8bf1e3d6d92dbd17c87
TJ <cooploops@gmail.com>
2018-09-27 21:59:09 -0700

    add conditional view after login and add input for file upload

--- src/pages/Admin/admin.js
+++ src/pages/Admin/admin.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import { Button, Form, Container } from 'semantic-ui-react';
+import { Button, Form, Container, Input } from 'semantic-ui-react';
 import axios from 'axios';
-import firebase from 'firebase';
+import firebaseApp from '../../base';
 
 class Admin extends Component {
 
     state={
         email:'',
-        password:''
+        password:'',
+        authenticated: false,
+        currentUser: null
     }
+    fileInput = React.createRef();
 
     handleSubmit = (e) => {
+        const {email, password} = this.state;
         e.preventDefault();
-        axios.post()
+        firebaseApp.auth().signInWithEmailAndPassword(email, password)
+            .catch(error => {
+                console.log(error);
+            });
 
         //clear form state
         this.setState({
             email: '',
             password: ''
         });
+    }
+
+    handleFileSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.fileInput.current.files[0]);
+        console.log(e.target);
     }
 
     handleInputChange = (e) => {
@@ -32,24 +45,58 @@ class Admin extends Component {
         console.log(this.state);
     }
 
+    componentWillMount() {
+        firebaseApp.auth().onAuthStateChanged(user => {
+            if(user) {
+                this.setState({
+                    authenticated: true,
+                    currentUser: user
+                })
+            } else {
+                this.setState({
+                    authenticated: false,
+                    currentUser: null
+                })
+            }
+        })
+    }
+
     render() {
-        const { email, password } = this.state;
-        
-        return (
-            <Container>
-                <Form onSubmit={this.handleSubmit}>
-                    <Form.Field required>
-                        <label>Email</label>
-                        <input type="email" required placeholder="Email" name='email' value={email} onChange={this.handleInputChange}/>
-                    </Form.Field>
-                    <Form.Field required>
-                        <label>Password</label>
-                        <input type="password" required placeholder="Password" name='password' value={password} onChange={this.handleInputChange}/>
-                    </Form.Field>
-                    <Button primary type='submit' disabled={!this.state.email || !this.state.password}>Submit</Button>
-                </Form>
-            </Container>
-        )
+        const { email, password, authenticated, currentUser } = this.state;
+
+        if(!authenticated) {
+            return (
+                <Container>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Form.Field required>
+                            <label>Email</label>
+                            <input type="email" required placeholder="Email" name='email' value={email} onChange={this.handleInputChange}/>
+                        </Form.Field>
+                        <Form.Field required>
+                            <label>Password</label>
+                            <Input type="password" required placeholder="Password" name='password' value={password} onChange={this.handleInputChange}/>
+                        </Form.Field>
+                        <Button primary type='submit' disabled={!this.state.email || !this.state.password}>Submit</Button>
+                    </Form>
+                </Container>
+            )
+        }
+        if(authenticated && currentUser) {
+            return (
+                <Container>
+                    <p>Up here will go a view of all the files uploaded</p>
+                    <p>Will also need a button to Delete and Edit</p>
+                    <p>Will include instructions on naming format for files as well here</p>
+                    <Form onSubmit={this.handleFileSubmit}>
+                        <Form.Field>
+                            <label>Upload File(s)</label>
+                            <input type="file" id="file" name="file" ref={this.fileInput}/>
+                        </Form.Field>
+                        <Button primary type='submit'>Upload</Button>
+                    </Form>
+                </Container>
+            )
+        }
     }
 }
 
