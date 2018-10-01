f64b391803659f60b5bfef303dc1f55faa7c620b
TJ <cooploops@gmail.com>
2018-10-01 01:14:42 -0700

    add upload functionality, add progress bar, need to show list next

--- src/pages/Admin/admin.js
+++ src/pages/Admin/admin.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Button, Form, Container, Input } from 'semantic-ui-react';
+import { Button, Form, Container, Input, Progress } from 'semantic-ui-react';
 import axios from 'axios';
 import firebaseApp from '../../base';
+import firebase from 'firebase';
 
 class Admin extends Component {
 
@@ -9,9 +10,16 @@ class Admin extends Component {
         email:'',
         password:'',
         authenticated: false,
-        currentUser: null
+        currentUser: null,
+        uploadBtnDisabled: true,
+        uploadState: '',
+        uploadStart: false,
+        uploadSuccess: false,
+        showProgressBar: false,
+        percentComplete: 0
     }
     fileInput = React.createRef();
+    uploadTask;
 
     handleSubmit = (e) => {
         const {email, password} = this.state;
@@ -30,8 +38,44 @@ class Admin extends Component {
 
     handleFileSubmit = (e) => {
         e.preventDefault();
-        console.log(this.fileInput.current.files[0]);
-        console.log(e.target);
+
+        this.uploadTask = this.storageRefPoint.child(`/user/${this.state.currentUser.uid}/podcasts`).put(this.fileInput.current.files[0]);
+        this.uploadTask.on('state_changed', (snapshot) => {
+            const percentProgress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            this.setState({
+                showProgressBar: true,
+                percentComplete: percentProgress
+            });
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.RUNNING:
+                this.setState({
+                    uploadState: 'Upload in Progress',
+                    uploadStart: true
+                });
+                break;
+                case firebase.storage.TaskState.PAUSED:
+                this.setState({
+                    uploadState: 'Upload Pause'
+                });
+                break;
+                case firebase.storage.TaskState.CANCELED:
+                this.setState({
+                    uploadState: 'Upload Cancelled'
+                });
+                break;
+                case firebase.storage.TaskState.SUCCESS:
+                this.setState({
+                    uploadState: 'Upload Complete',
+                    uploadSuccess: true
+                });
+                break;
+            }
+        }, (error) => {
+            console.log(error);
+            alert('an error occurred during upload, let TJ know so he can investigate');
+        }, () => {
+            console.log('upload complete');
+        });
     }
 
     handleInputChange = (e) => {
@@ -42,16 +86,22 @@ class Admin extends Component {
         this.setState({
             [name]: value
         });
-        console.log(this.state);
+    }
+
+    signOut = () => {
+        firebaseApp.auth().signOut().catch(error => {
+            console.log(error);
+        })
     }
 
     componentWillMount() {
+        firebaseApp.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
         firebaseApp.auth().onAuthStateChanged(user => {
             if(user) {
                 this.setState({
                     authenticated: true,
                     currentUser: user
-                })
+                });
             } else {
                 this.setState({
                     authenticated: false,
@@ -60,11 +110,35 @@ class Admin extends Component {
             }
         })
     }
+    
+    firebaseStorage = firebaseApp.storage();
+    storageRefPoint = this.firebaseStorage.ref();
 
     render() {
         const { email, password, authenticated, currentUser } = this.state;
 
-        if(!authenticated) {
+        if(authenticated && currentUser) {
+            return (
+                <Container>
+                    <Button primary onClick={this.signOut}>Sign Out</Button>
+                    <p>Up here will go a view of all the files uploaded</p>
+                    <p>Will also need a button to Delete and Edit</p>
+                    <p>Will include instructions on naming format for files as well here</p>
+                    {this.state.uploadStart &&
+                        <Progress percent={this.state.percentComplete} indicating success={this.state.uploadSuccess} progress={this.state.percentComplete}>
+                            {this.state.uploadState}
+                        </Progress>
+                    }
+                    <Form onSubmit={this.handleFileSubmit}>
+                        <Form.Field>
+                            <label>Upload File(s)</label>
+                            <input type="file" id="file" name="file" ref={this.fileInput} onChange={() => this.setState({uploadBtnDisabled: false})}/>
+                        </Form.Field>
+                        <Button primary type='submit' disabled={this.state.uploadBtnDisabled}>Upload</Button>
+                    </Form>
+                </Container>
+            )
+        } else {
             return (
                 <Container>
                     <Form onSubmit={this.handleSubmit}>
@@ -77,22 +151,6 @@ class Admin extends Component {
                             <Input type="password" required placeholder="Password" name='password' value={password} onChange={this.handleInputChange}/>
                         </Form.Field>
                         <Button primary type='submit' disabled={!this.state.email || !this.state.password}>Submit</Button>
-                    </Form>
-                </Container>
-            )
-        }
-        if(authenticated && currentUser) {
-            return (
-                <Container>
-                    <p>Up here will go a view of all the files uploaded</p>
-                    <p>Will also need a button to Delete and Edit</p>
-                    <p>Will include instructions on naming format for files as well here</p>
-                    <Form onSubmit={this.handleFileSubmit}>
-                        <Form.Field>
-                            <label>Upload File(s)</label>
-                            <input type="file" id="file" name="file" ref={this.fileInput}/>
-                        </Form.Field>
-                        <Button primary type='submit'>Upload</Button>
                     </Form>
                 </Container>
             )
