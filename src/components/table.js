e26cfb8a96a68148cad0d61d583a76196975796c
TJ <cooploops@gmail.com>
2018-10-25 22:39:31 -0700

    basic table shows with data on load, need to add detection of data added to db

--- src/components/table.js
+++ src/components/table.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import { Table, Button, Input } from 'semantic-ui-react';
+import { Table, Button } from 'semantic-ui-react';
+
+const handleClick = () => {console.log('i work')}
 
 const table = props => {
 
-
-    if (!props.data) {
+    if (!props.podcastdata) {
         return <div>no data detected or not connected to database</div>
     } else {
-        const tableRows = props.data.map(dataRow => {
+        const tableRows = props.podcastdata.map(dataRow => {
+            const data = dataRow.data();
             return (
                 <Table.Row>
-                    <Table.Cell collapsing><Button primary onClick={}>Edit</Button></Table.Cell>
-                    <Table.Cell>{dataRow.title}</Table.Cell>
-                    <Table.Cell>{dataRow.episode}</Table.Cell>
-                    <Table.Cell>{dataRow.description}</Table.Cell>
+                    <Table.Cell collapsing><Button primary onClick={handleClick}>Edit</Button></Table.Cell>
+                    <Table.Cell>{data.title}</Table.Cell>
+                    <Table.Cell>{data.episode}</Table.Cell>
+                    <Table.Cell>{data.description}</Table.Cell>
                 </Table.Row>
-            )
-        })
+            );
+        });
+
+        return (
+            <Table basic compact definition>
+                <Table.Header fullWidth>
+                    <Table.Row>
+                        <Table.HeaderCell />
+                        <Table.HeaderCell>Title</Table.HeaderCell>
+                        <Table.HeaderCell>Episode Number</Table.HeaderCell>
+                        <Table.HeaderCell>Description</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {tableRows}
+                </Table.Body>
+            </Table>
+        )
     }
 
     
-    return (
-        <Table basic compact definition>
-            <Table.Header fullWidth>
-                <Table.Row>
-                    <Table.HeaderCell />
-                    <Table.HeaderCell>Title</Table.HeaderCell>
-                    <Table.HeaderCell>Episode Number</Table.HeaderCell>
-                    <Table.HeaderCell>Description</Table.HeaderCell>
-                </Table.Row>
-            </Table.Header>
 
-            <Table.Body>
-                {tableRows}
-            </Table.Body>
-        </Table>
-    )
 }
 
 export default table;
